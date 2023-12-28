a3cd7dd08640538e789b0dbf4686e7eff2abee19
david-alves-da-silva <212390alves@gmail.com>
2023-12-28 11:15:37 +0100

    databaseError

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -3,21 +3,15 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
-  NotFoundException,
 } from '@nestjs/common';
 import { Observable, catchError } from 'rxjs';
-import { NotFoundError } from '../types/NotFoundError';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
-        if (error instanceof NotFoundError) {
-          throw new NotFoundException(error.message);
-        } else {
-          throw error;
-        }
+        throw error;
       }),
     );
   }
