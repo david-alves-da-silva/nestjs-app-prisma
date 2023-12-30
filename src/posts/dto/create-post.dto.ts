7b6b3987e86bb9637deb4ed1d3f735863831e3b9
david-alves-da-silva <212390alves@gmail.com>
2023-12-30 11:33:21 +0100

    deploy

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ description: 'Título do post do usuário.' })
   @IsString()
   @IsNotEmpty()
   title: string;
+  @ApiProperty({ description: 'Conteúdo do post.' })
   @IsString()
   @IsOptional()
   content?: string;
+  @ApiProperty({ description: 'Email do autor do post.' })
   @IsEmail()
   authorEmail: string;
 }
